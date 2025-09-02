c9f3730fe86a07e11c72a8ecdfecf8a19854a07f
aleaprilian <aprilianale@gmail.com>
2025-09-03 00:32:34 +0700

    Add files via upload

---
+++ Praktikum2.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/*
+ @author Ale Aprilian
+ */
+package com.mycompany.praktikum2;
+public class Praktikum2 {
+    public static void main(String[] args) {
+       /*
+        
+        */ 
+       
+        //Menampilkan   hello world
+        System.out.println("Hello World");
+        //Menampilkan Nama
+        System.out.println("Halo Nama Saya Ale Aprilian");
+     
+         
+ 
+        /*
+        Deklarasi dan inisailisai
+        */
+        final String NAMA = "Ale Aprilian";     //inisialisasi variabel konstanta
+        char golDarah = 'O';
+        int umur = 21;
+     /*
+        Tipe data
+        */
+     String Alamat = " Taman Karya VI ";
+     String Nim = "2407114008";
+     double[] ipk = {3.14,3.45,3.16};
+     byte ipkSemester = (byte) ipk[0];
+     byte newIpk = ipkSemester;     
+   
+    
+     System.out.println("Nama Saya : " + NAMA);
+     System.out.println("Nim Saya : " + Nim);
+     System.out.println("Golongan Darah Saya : " + golDarah );
+     System.out.println("Umur : " + umur);
+     //System.out.println(ipk[2]);
+     System.out.println("Alamat Saya : " + Alamat);
+     System.out.println("Nilai Ipk : " + newIpk);
+     
+     
+     
+
+    }
+      
+     
+    }
+        
+
