50f51e92cb4cf3697fe9557a601a5f7929d23e49
gitaazzahra1 <gitaazzahra0206@gmail.com>
2025-10-14 08:09:18 +0700

    Create BarangElektronik.java

---
+++ praktikum7/BarangElektronik.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.praktikum7;
+
+public class BarangElektronik extends Produk implements HargaAkhir {
+    private int garansi;
+   
+    public BarangElektronik (String nama, int harga, int garansi){
+        super (nama, harga);
+        this.garansi = garansi;
+    }
+    @Override
+    public void tampilkanInfo(){
+        System.out.println("nama produk: " + nama);
+        System.out.println("harga: " + harga);
+        System.out.println("garansi: " + garansi);
+    }
+    public void tampilkanGaransi (){
+        System.out.println("Garansi: " + garansi + " bulan");
+    }
+    
+    @Override
+    public double hitungPajak(){
+        return harga * 0.01;
+    }
+    @Override
+    public double hitungHarga(){
+        return harga * 1.01;
+    }
+}
