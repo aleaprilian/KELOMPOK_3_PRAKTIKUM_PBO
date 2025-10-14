53b9ff0b726b14a2302a6eb31eae52d662aba9da
gitaazzahra1 <gitaazzahra0206@gmail.com>
2025-10-14 08:10:00 +0700

    Create Produk.java

---
+++ praktikum7/Produk.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.praktikum7;
+
+public class Produk implements HargaAkhir {
+    protected String nama;
+    protected int harga;
+    
+    public Produk (String nama, int harga){
+        this.nama = nama;
+        this.harga = harga;
+    }
+    public void tampilkanInfo(){
+        System.out.println("Nama produk: " + nama);
+        System.out.println("Harga Produk: " + harga);
+    }
+    
+    public double hitungPajak(){
+        return harga * 0.05;
+    }
+    @Override
+    public double  hitungHarga(){
+        return harga * 1.05;
+    }
+}
