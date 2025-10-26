27c3ca023396f70c71e767ed0b93ac21728a33a7
rajamfaridztalbi <rajamfaridztalbi@gmail.com>
2025-10-26 15:40:12 +0700

    Add files via upload

---
+++ TugasPraktikum8/src/main/java/com/mycompany/tugaspraktikum8/TugasPraktikum8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugaspraktikum8;
+
+/**
+ *
+ * @author Faridz Talbi
+ */
+public class TugasPraktikum8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
